e336112f4a10582a9aa41c6958c8b8924fd491b2
Anthonypradosolis <apradosolis@danielcastelao.org>
2023-12-05 17:18:31 +0100

    Boletin02_01

---
+++ Boletin02/Boletin02_01/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // pedir datos
+        float base= 4, altura= 3, area;
+        area= base*altura/2;
+        System.out.println("area del triangulo ="+area+ " metros");
+    }
+}
