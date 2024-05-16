f602accb198700482920fc3e55f7bc396c99ec5f
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-05-16 14:28:59 +0200

    Boletin 36 Base de Datos

---
+++ Boletin36/Boletin36_01/src/LibraryGUI.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+public class LibraryGUI {
+    private Database db;
+
+    public LibraryGUI() {
+        db = new Database();
+        // código para crear la interfaz gráfica
+
+    }
+
+    // métodos para manejar los eventos de los botones y formularios
+    public void onAddLibro(ActionEvent e) {
+        // código para agregar un libro
+    }
+}
